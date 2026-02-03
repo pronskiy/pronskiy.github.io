bb252d8972181a9b65bd51d1201a18f22b72c973
Roman Pronskiy <roman@pronskiy.com>
2026-02-03 13:05:09 +0100

    Fix serve

--- serve.sh
+++ serve.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Cleanup both processes on exit
-cleanup() {
-    kill $TAILWIND_PID $SCULPIN_PID 2>/dev/null
-}
-trap cleanup EXIT
+# Kill entire process tree on exit
+trap 'kill 0' INT TERM EXIT
 
 # Run Tailwind with prefixed output
-(npx tailwind -i assets/css/app.css -o source/assets/css/app.css --watch 2>&1 | awk '{print "[tailwind] " $0; fflush()}') &
-TAILWIND_PID=$!
+npx tailwind -i assets/css/app.css -o source/assets/css/app.css --watch 2>&1 | awk '{print "[tailwind] " $0; fflush()}' &
 
 # Run Sculpin with prefixed output
-(./vendor/bin/sculpin generate --watch --server 2>&1 | awk '{print "[sculpin] " $0; fflush()}') &
-SCULPIN_PID=$!
+./vendor/bin/sculpin generate --watch --server 2>&1 | awk '{print "[sculpin] " $0; fflush()}' &
 
 # Wait for both
 wait
